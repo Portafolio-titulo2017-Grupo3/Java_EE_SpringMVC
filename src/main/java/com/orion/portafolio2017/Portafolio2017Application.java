a58898a9d67dbc6de06663b436820c3feaaf82f3
sotox1988 <matias.sotomayor@gmail.com>
2017-10-23 15:28:23 -0300

    Inicio del proyecto web Java portafolio 2017

---
+++ src/main/java/com/orion/portafolio2017/Portafolio2017Application.java
@@ -0,0 +1,12 @@
+package com.orion.portafolio2017;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portafolio2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Portafolio2017Application.class, args);
+	}
+}
