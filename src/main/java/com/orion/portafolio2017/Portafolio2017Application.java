0d5caffa8cdc72261908b939aeec34d165000790
sotox1988 <matias.sotomayor@gmail.com>
2017-11-01 12:38:25 -0300

    Se cambia el nombre del proyecto a Java EE SpringMVC

--- src/main/java/com/orion/portafolio2017/Portafolio2017Application.java
+++ src/main/java/com/orion/portafolio2017/Portafolio2017Application.java
@@ -14,6 +14,7 @@ public class Portafolio2017Application extends SpringBootServletInitializer{
     }
 	
 	public static void main(String[] args) {
+		//SpringApplication.run(Portafolio2017Application.class, args).close();
 		SpringApplication.run(Portafolio2017Application.class, args);
 	}
 }
