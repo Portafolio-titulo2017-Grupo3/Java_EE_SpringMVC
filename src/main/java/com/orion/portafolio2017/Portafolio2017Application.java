3e430410cd506b108b7833fccc0fa099a4a8ee72
sotox1988 <matias.sotomayor@gmail.com>
2017-10-31 23:30:24 -0300

    La aplicacion esta funcionando en Servidor

--- src/main/java/com/orion/portafolio2017/Portafolio2017Application.java
+++ src/main/java/com/orion/portafolio2017/Portafolio2017Application.java
@@ -2,10 +2,17 @@ package com.orion.portafolio2017;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Portafolio2017Application {
+public class Portafolio2017Application extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Portafolio2017Application.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Portafolio2017Application.class, args);
 	}
