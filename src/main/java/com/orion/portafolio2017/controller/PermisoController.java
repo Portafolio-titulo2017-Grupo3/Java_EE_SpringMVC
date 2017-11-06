c074089547f9b8812776d84c72c6b5bac89f0856
sotox1988 <matias.sotomayor@gmail.com>
2017-11-06 12:31:04 -0300

    Se agrego clase controller PermisoController

---
+++ src/main/java/com/orion/portafolio2017/controller/PermisoController.java
@@ -0,0 +1,10 @@
+package com.orion.portafolio2017.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/permisos")
+public class PermisoController {
+
+}
