e75885321fab70a98dc37683411b47fbc827ea3c
sotox1988 <matias.sotomayor@gmail.com>
2017-11-12 19:57:13 -0300

    Se agrego Nada :)

--- src/main/java/converterBackup/UsuarioConverter.java
+++ src/main/java/converterBackup/UsuarioConverter.java
@@ -3,15 +3,7 @@ package converterBackup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
-import com.orion.portafolio2017.entity.Cargo;
-import com.orion.portafolio2017.entity.Departamento;
-import com.orion.portafolio2017.entity.Permiso;
 import com.orion.portafolio2017.entity.Usuario;
-
-import modelBackup.CargoModel;
-import modelBackup.DepartamentoModel;
-import modelBackup.PermisoModel;
 import modelBackup.UsuarioModel;
 
 
@@ -22,9 +14,6 @@ public class UsuarioConverter {
 	@Qualifier("funcionarioConverter")
 	private FuncionarioConverter funcionarioConverter;
 	
-	@Autowired
-	@Qualifier("perfilConverter")
-	private PerfilConverter perfilConverter;
 
 	public Usuario convertUsuarioModel2Usuario(UsuarioModel usuarioModel) {
 
@@ -32,7 +21,6 @@ public class UsuarioConverter {
 		usuario.setIdUsuario(usuarioModel.getIdUsuario());
 		usuario.setUsername(usuarioModel.getUsername());
 		usuario.setPassword(usuarioModel.getPassword());
-		usuario.setPerfil(perfilConverter.convertPerfilModel2Perfil(usuarioModel.getPerfil()));
 		usuario.setFuncionario(funcionarioConverter.convertFuncionarioModel2Funcionario(usuarioModel.getFuncionario()));
 		return usuario;
 		
@@ -45,7 +33,6 @@ public class UsuarioConverter {
 		usuarioModel.setIdUsuario(usuario.getIdUsuario());
 		usuarioModel.setUsername(usuario.getUsername());
 		usuarioModel.setPassword(usuario.getPassword());
-		usuarioModel.setPerfil(perfilConverter.convertPerfil2PerfilModel(usuario.getPerfil()));
 		usuarioModel.setFuncionario(funcionarioConverter.convertFuncionario2FuncionarioModel(usuario.getFuncionario()));
 		return usuarioModel;
 		
